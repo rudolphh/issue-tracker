08cf21b82eef47eaa8b5931bd7c3a8be527fc36a
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-07-16 12:00:02 -0700

    Update initial before user stories

--- routes/api.js
+++ routes/api.js
@@ -17,25 +17,25 @@ const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRIN
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
+
     .get(function (req, res){
       var project = req.params.project;
-      
+
     })
-    
+
     .post(function (req, res){
       var project = req.params.project;
       
     })
-    
+
     .put(function (req, res){
       var project = req.params.project;
-      
+
     })
-    
+
     .delete(function (req, res){
       var project = req.params.project;
-      
+
     });
-    
+
 };
